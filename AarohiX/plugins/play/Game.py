e1537e4a487e751c0b852f11efd80e6d224787fc
israaa <88703388+AlSayeb@users.noreply.github.com>
2024-06-07 05:27:29 +0300

    Add Game.py

---
+++ AarohiX/plugins/play/Game.py
@@ -0,0 +1,47 @@
+from pyrogram import Client, enums, filters
+import asyncio
+from AarohiX import app
+
+from pyrogram.handlers import MessageHandler
+
+  
+@app.on_message(filters.command("نبله", prefixes=""))
+async def dart(bot, message):
+    x=await bot.send_dice(message.chat.id, "🎯")
+    m=x.dice.value
+    await message.reply_text(f"Hey {message.from_user.mention} your Score is : {m}",quote=True)
+
+@app.on_message(filters.command("باسكت", prefixes=""))
+async def basket(bot, message):
+    x=await bot.send_dice(message.chat.id, "🏀")
+    m=x.dice.value
+    await message.reply_text(f"Hey {message.from_user.mention} your Score is : {m}",quote=True)
+    
+@app.on_message(filters.command("اسبن", prefixes=""))
+async def basket(bot, message):
+    x=await bot.send_dice(message.chat.id, "🎰")
+    m=x.dice.value
+    await message.reply_text(f"Hey {message.from_user.mention} your Score is : {m}",quote=True)
+    
+@app.on_message(filters.command("بولنج", prefixes=""))
+async def basket(bot, message):
+    x=await bot.send_dice(message.chat.id, "🎳")
+    m=x.dice.value
+    await message.reply_text(f"Hey {message.from_user.mention} your Score is : {m}",quote=True)
+    
+@app.on_message(filters.command("قدم", prefixes=""))
+async def basket(bot, message):
+    x=await bot.send_dice(message.chat.id, "⚽")
+    m=x.dice.value
+    await message.reply_text(f"Hey {message.from_user.mention} your Score is : {m}",quote=True)
+__help__ = """
+ Play Game With Emojis:
+/dice - Dice 🎲
+/dart - Dart 🎯
+/basket - Basket Ball 🏀
+/ball - Bowling Ball 🎳
+/football - Football ⚽
+/jackpot - Spin slot machine 🎰
+ """
+
+__mod_name__ = "Dɪᴄᴇ"
